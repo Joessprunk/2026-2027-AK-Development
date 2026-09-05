68f7a78309876c639878b14625f5e84c8b8bdba7
Jozef Sprunk <Jozefsp27@gmail.com>
2026-09-04 21:17:28 -0400

    Update DriveConstants.java

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -17,8 +17,8 @@ import edu.wpi.first.math.util.Units;
 public class DriveConstants {
   public static final double maxSpeedMetersPerSec = 5.0;
   public static final double odometryFrequency = 100.0; // Hz
-  public static final double trackWidth = Units.inchesToMeters(26.5);
-  public static final double wheelBase = Units.inchesToMeters(26.5);
+  public static final double trackWidth = Units.inchesToMeters(27.0);
+  public static final double wheelBase = Units.inchesToMeters(27.0);
   public static final double driveBaseRadius = Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
   public static final Translation2d[] moduleTranslations =
       new Translation2d[] {
@@ -35,22 +35,23 @@ public class DriveConstants {
   public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
 
   // Device CAN IDs
-  public static final int pigeonCanId = 9;
+  public static final int pigeonCanId = 14;
 
-  public static final int frontLeftDriveCanId = 1;
-  public static final int backLeftDriveCanId = 3;
-  public static final int frontRightDriveCanId = 5;
+  public static final int frontLeftAbsoluteId = 4;
+  public static final int frontLeftDriveCanId = 8;
+  public static final int frontLeftTurnCanId = 12;
+
+  public static final int backLeftAbsoluteId = 5;
+  public static final int backLeftDriveCanId = 9;
+  public static final int backLeftTurnCanId = 13;
+
+  public static final int frontRightAbsoluteId = 2;
+  public static final int frontRightDriveCanId = 6;
+  public static final int frontRightTurnCanId = 10;
+
+  public static final int backRightAbsoluteId = 3;
   public static final int backRightDriveCanId = 7;
-
-  public static final int frontLeftTurnCanId = 2;
-  public static final int backLeftTurnCanId = 4;
-  public static final int frontRightTurnCanId = 6;
-  public static final int backRightTurnCanId = 8;
-
-  public static final int frontLeftAbsoluteId = 0;
-  public static final int frontRightAbsoluteId = 0;
-  public static final int backLeftAbsoluteId = 0;
-  public static final int backRightAbsoluteId = 0;
+  public static final int backRightTurnCanId = 11;
 
   // Drive motor configuration
   public static final int driveMotorCurrentLimit = 50;
@@ -68,10 +69,10 @@ public class DriveConstants {
   // Wheel Rad/Sec
 
   // Drive PID configuration
-  public static final double driveKp = 0.0;
+  public static final double driveKp = 0.18;
   public static final double driveKd = 0.0;
-  public static final double driveKs = 0.0;
-  public static final double driveKv = 0.1;
+  public static final double driveKs = 1.11607;
+  public static final double driveKv = 2.9583;
   public static final double driveSimP = 0.05;
   public static final double driveSimD = 0.0;
   public static final double driveSimKs = 0.0;
@@ -89,15 +90,15 @@ public class DriveConstants {
   public static final double turnEncoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
 
   // Turn PID configuration
-  public static final double turnKp = 2.0;
-  public static final double turnKd = 0.0;
+  public static final double turnKp = 0.45;
+  public static final double turnKd = 0.15;
   public static final double turnSimP = 8.0;
   public static final double turnSimD = 0.0;
   public static final double turnPIDMinInput = 0; // Radians
   public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
 
   // PathPlanner configuration
-  public static final double robotMassKg = 74.088;
+  public static final double robotMassKg = 61.0;
   public static final double robotMOI = 6.883;
   public static final double wheelCOF = 1.2;
   public static final RobotConfig ppConfig =
